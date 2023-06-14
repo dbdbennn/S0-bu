6d0271c0f982332657dbc540d72dd2416bc06763
hyewon <w2106@e-mirim.hs.kr>
2023-06-14 10:30:13 +0900

    firebase에 문서 생성하여 이메일 저장

--- src/socket/socketServer.js
+++ src/socket/socketServer.js
@@ -10,6 +10,14 @@ const io = new Server(server, {
   }
 });
 
+// Firebase 초기화 및 Firestore 가져오기
+const admin = require('firebase-admin');
+const serviceAccount = require('../../study-0-c5302-firebase-adminsdk-w31t0-a72b9187d1.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+const db = admin.firestore();
+
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', (socket) => {
@@ -25,6 +33,35 @@ io.on('connection', (socket) => {
     // Firebase Authentication에서 저장된 이메일 가져오기
     const userEmail = userInfo.email;
     console.log('User email:', userEmail);
+
+    // Firestore에 문서 생성 또는 가져오기
+    //.doc(문서명)
+    const studyroomRef = db.collection('studyroom').doc('test2');
+    studyroomRef.get()
+      .then((doc) => {
+        if (doc.exists) {
+          // 문서가 이미 존재하는 경우, 이메일 필드 업데이트
+          studyroomRef.update({ email: userEmail })
+            .then(() => {
+              console.log('User email updated in Firestore');
+            })
+            .catch((error) => {
+              console.error('Error updating user email:', error);
+            });
+        } else {
+          // 문서가 존재하지 않는 경우, 새로운 문서 생성 및 이메일 필드 저장
+          studyroomRef.set({ email: userEmail })
+            .then(() => {
+              console.log('User email saved in Firestore');
+            })
+            .catch((error) => {
+              console.error('Error saving user email:', error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.error('Error getting studyroom document:', error);
+      });
 
     // 현재 접속 중인 모든 클라이언트에게 이메일 정보 전달
     io.emit('userEmail', userEmail);
