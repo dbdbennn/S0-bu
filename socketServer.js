cb361678c389c922508f4fd2238468e0e4126c3c
Yu Jung Eun <85720276+dbdbennn@users.noreply.github.com>
2023-06-19 17:01:00 +0900

    소켓 수정 -양혜원-

--- socketServer.js
+++ socketServer.js
@@ -1,98 +1,119 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
 
 // Firebase 문서 참조 변수
 let studyroomRefs = [];
 
 // Firebase 초기화 및 Firestore 가져오기
-const admin = require('firebase-admin');
-const serviceAccount = require('./study-0-c5302-firebase-adminsdk-w31t0-a72b9187d1.json');
+const admin = require("firebase-admin");
+const serviceAccount = require("./study-0-c5302-firebase-adminsdk-w31t0-a72b9187d1.json");
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 const db = admin.firestore();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+
     if (studyroomRefs.length > 0) {
       studyroomRefs.forEach((studyroomRef) => {
         // Firebase Firestore에서 해당 필드 제거
-        studyroomRef.update({ email: admin.firestore.FieldValue.delete() })
+        studyroomRef
+          .update({ uids: admin.firestore.FieldValue.arrayRemove(socket.id) })
           .then(() => {
-            console.log('User email removed from Firestore');
+            console.log("User uid removed from Firestore");
           })
           .catch((error) => {
-            console.error('Error removing user email from Firestore:', error);
+            console.error("Error removing user uid from Firestore:", error);
           });
       });
     }
   });
 
-  socket.on('userConnected', (userInfo) => {
-    console.log('Received user information:', userInfo);
-  
+  socket.on("userConnected", (userInfo) => {
+    console.log("Received user information:", userInfo);
+
     // Firebase Authentication에서 저장된 이메일 가져오기
-    const userEmail = userInfo.email;
-    console.log('User email:', userEmail);
-  
+    const userUID = userInfo.uid;
+    console.log("User uid:", userUID);
+
     // 페이지 경로 가져오기
     const pagePath = userInfo.pagePath;
-    console.log('Page path:', pagePath);
-  
+    console.log("Page path:", pagePath);
+
     // Firestore에 문서 생성 또는 가져오기
-    const studyroomRef = db.collection('studyroom').doc(pagePath);
+    const studyroomRef = db.collection("studyroom").doc(pagePath);
     studyroomRefs.push(studyroomRef); // 배열에 참조 추가
 
-    studyroomRef.get()
+    studyroomRef
+      .get()
       .then((doc) => {
         if (doc.exists) {
           // 문서가 이미 존재하는 경우, 이메일 필드 업데이트
-          studyroomRef.update({ email: admin.firestore.FieldValue.arrayUnion(userEmail) })
+          studyroomRef
+            .update({ uids: admin.firestore.FieldValue.arrayUnion(userUID) })
             .then(() => {
-              console.log('User email updated in Firestore');
+              console.log("User uid updated in Firestore");
             })
             .catch((error) => {
-              console.error('Error updating user email:', error);
+              console.error("Error updating user uid:", error);
             });
         } else {
           // 문서가 존재하지 않는 경우, 새로운 문서 생성 및 이메일 필드 저장
-          studyroomRef.set({ email: [userEmail] })
+          studyroomRef
+            .set({ uids: [userUID] })
             .then(() => {
-              console.log('User email saved in Firestore');
+              console.log("User uid saved in Firestore");
             })
             .catch((error) => {
-              console.error('Error saving user email:', error);
+              console.error("Error saving user uid:", error);
             });
         }
       })
       .catch((error) => {
-        console.error('Error getting studyroom document:', error);
+        console.error("Error getting studyroom document:", error);
       });
 
     // 현재 접속 중인 모든 클라이언트에게 이메일 정보 전달
-    io.emit('userEmail', userEmail);
+    io.emit("userUID", userUID);
+
+    // 현재 저장된 모든 사용자 정보 가져오기
+    if (studyroomRefs.length > 0) {
+      const allUsers = [];
+      studyroomRefs.forEach((studyroomRef) => {
+        studyroomRef.get().then((doc) => {
+          if (doc.exists) {
+            const users = doc.data().uids || []; // users가 undefined인 경우 빈 배열로 초기화
+            allUsers.push(...users);
+          }
+
+          // 중복 제거 후 현재 사용자에게 모든 사용자 정보 전달
+          const uniqueUsers = [...new Set(allUsers)];
+          socket.emit("allUsers", uniqueUsers);
+        });
+      });
+    }
   });
 
-  socket.on('getUserInfo', (socketId) => {
+  socket.on("getUserInfo", (socketId) => {
     console.log(socketId);
   });
 });
 
 server.listen(4000, () => {
-  console.log('listening on *:4000');
+  console.log("listening on *:4000");
 });
--- src/pages/community.js
+++ src/pages/community.js
@@ -1,15 +1,23 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { useRouter } from 'next/router';
-import logo from '../../public/images/logo.png';
-import styles from '../styles/community.module.css';
-import navStyles from '../styles/nav.module.css';
-import Image from 'next/image';
-import firebase from 'firebase/app';
-import db from '../net/db';
-import { getDocs, collection, query, orderBy, getFirestore, setDoc, serverTimestamp } from 'firebase/firestore';
-import { doc, getDoc } from 'firebase/firestore';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import io from 'socket.io-client';
+import React, { useState, useEffect, useRef } from "react";
+import { useRouter } from "next/router";
+import logo from "../../public/images/logo.png";
+import styles from "../styles/community.module.css";
+import navStyles from "../styles/nav.module.css";
+import Image from "next/image";
+import firebase from "firebase/app";
+import db from "../net/db";
+import {
+  getDocs,
+  collection,
+  query,
+  orderBy,
+  getFirestore,
+  setDoc,
+  serverTimestamp,
+} from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import io from "socket.io-client";
 
 const useOnClickOutside = (ref, handler) => {
   useEffect(() => {
@@ -30,17 +38,16 @@ const useOnClickOutside = (ref, handler) => {
 
 function Community() {
   const [showModal, setShowModal] = useState(false);
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
-  const [roomID, setRoomID] = useState('');
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [roomID, setRoomID] = useState("");
   const [roomIdError, setRoomIdError] = useState(false);
   const [missingFieldsError, setMissingFieldsError] = useState(false);
   const [posts, setPosts] = useState([]);
   const [loggedIn, setLoggedIn] = useState(false);
-  const [nickname, setNickname] = useState('');
-  const [email, setEmail] = useState('');
-  const [displayName, setDisplayName] = useState('');
-
+  const [nickname, setNickname] = useState("");
+  const [email, setEmail] = useState("");
+  const [displayName, setDisplayName] = useState("");
 
   const router = useRouter();
   const modal = useRef(null);
@@ -50,7 +57,7 @@ function Community() {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         setLoggedIn(true);
-        console.log('로그인 상태: 로그인됨' + user.uid);
+        console.log("로그인 상태: 로그인됨" + user.uid);
 
         // 사용자 정보 가져오기
         const { displayName, email } = user;
@@ -62,7 +69,7 @@ function Community() {
         // socket.emit('userConnected', { displayName, email, pagePath });
       } else {
         setLoggedIn(false);
-        console.log('로그인 상태: 로그인되지 않음');
+        console.log("로그인 상태: 로그인되지 않음");
       }
     });
 
@@ -71,7 +78,7 @@ function Community() {
 
   // 현재 접속한 이메일 출력
   useEffect(() => {
-    console.log('User email in Community:', email);
+    console.log("User uid in Community:", email);
   }, [email]);
 
   // modal
@@ -110,7 +117,7 @@ function Community() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (title.trim() === '' || content.trim() === '' || roomID.trim() === '') {
+    if (title.trim() === "" || content.trim() === "" || roomID.trim() === "") {
       setMissingFieldsError(true);
       return;
     }
@@ -118,7 +125,7 @@ function Community() {
     const firestore = getFirestore();
 
     // 해당 roomID가 이미 Firestore에 존재하는지 확인
-    const roomRef = doc(firestore, 'posts', roomID);
+    const roomRef = doc(firestore, "posts", roomID);
     const roomDoc = await getDoc(roomRef);
 
     if (roomDoc.exists()) {
@@ -133,24 +140,24 @@ function Community() {
     const date = today.getDate(); // 날짜
     const datestr = `${year}/${month}/${date}`;
 
-    await setDoc(doc(db, 'posts', roomID), {
+    await setDoc(doc(db, "posts", roomID), {
       title,
       content,
       roomID,
       date: datestr, // 날짜 저장
-      timestamp: today
+      timestamp: today,
     });
 
-    setTitle('');
-    setContent('');
-    setRoomID('');
+    setTitle("");
+    setContent("");
+    setRoomID("");
     setRoomIdError(false);
     setMissingFieldsError(false);
     setShowModal(false);
 
     // Firestore에서 게시물 데이터 불러오기
-    const postsCollectionRef = collection(db, 'posts');
-    const q = query(postsCollectionRef, orderBy('timestamp', 'desc')); // 내림차순으로 정렬
+    const postsCollectionRef = collection(db, "posts");
+    const q = query(postsCollectionRef, orderBy("timestamp", "desc")); // 내림차순으로 정렬
     const querySnapshot = await getDocs(q);
 
     const updatedPosts = [];
@@ -167,9 +174,9 @@ function Community() {
   let month = today.getMonth() + 1; // 월
   let date = today.getDate(); // 날짜
 
-  console.log(year + '/' + month + '/' + date);
+  console.log(year + "/" + month + "/" + date);
 
-  const datestr = year + '/' + month + '/' + date;
+  const datestr = year + "/" + month + "/" + date;
 
   useOnClickOutside(modal, () => {
     setShowModal(false);
@@ -179,7 +186,9 @@ function Community() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const querySnapshot = await getDocs(query(collection(db, 'posts'), orderBy('timestamp', 'desc')));
+        const querySnapshot = await getDocs(
+          query(collection(db, "posts"), orderBy("timestamp", "desc"))
+        );
         const data = querySnapshot.docs.map((doc) => doc.data());
         setPosts(data);
       } catch (error) {
@@ -190,53 +199,52 @@ function Community() {
     fetchData();
   }, []);
 
-  const roomIDInputStyles = roomIdError ? `${styles.inputError}` : '';
+  const roomIDInputStyles = roomIdError ? `${styles.inputError}` : "";
 
   const createPost = (post) => {
     const handlePostClick = () => {
       router.push({
         pathname: `/study`,
-        query: { roomID: post.roomID }
+        query: { roomID: post.roomID },
       });
     };
 
     return (
       <div className={styles.post} onClick={handlePostClick}>
-        <h1 className={styles['post-title']}>{post.title}</h1>
-        <p className={styles['post-writer']}>{nickname}</p>
-        <p className={styles['post-date']}>{datestr}</p>
-        <p className={styles['post-content']}>{post.content}</p>
+        <h1 className={styles["post-title"]}>{post.title}</h1>
+        <p className={styles["post-writer"]}>{nickname}</p>
+        <p className={styles["post-date"]}>{datestr}</p>
+        <p className={styles["post-content"]}>{post.content}</p>
       </div>
     );
   };
 
-
   // // 소켓 클라이언트 코드 시작점
   useEffect(() => {
-    const socket = io.connect('http://localhost:4000');
+    const socket = io.connect("http://localhost:4000");
 
     const auth = getAuth(firebase);
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         setLoggedIn(true);
-        console.log('로그인 상태: 로그인됨' + user.uid);
+        console.log("로그인 상태: 로그인됨" + user.uid);
 
         // 사용자 정보 가져오기
         const { displayName, email, uid } = user;
         setDisplayName(displayName);
-        setEmail(email);
+        setEmail(uid);
 
         // 사용자 정보를 서버로 전송
         const pagePath = router.pathname;
-        socket.emit('userConnected', { displayName, email, pagePath });
+        socket.emit("userConnected", { displayName, uid, pagePath });
       } else {
         setLoggedIn(false);
-        console.log('로그인 상태: 로그인되지 않음');
+        console.log("로그인 상태: 로그인되지 않음");
       }
     });
 
     // 서버로부터 전달된 이메일 정보를 받아와서 상태 업데이트
-    socket.on('userEmail', (userEmail) => {
+    socket.on("userEmail", (userEmail) => {
       setEmail(userEmail);
     });
 
@@ -246,15 +254,14 @@ function Community() {
   }, [router.pathname]);
   // // 소켓 클라이언트 코드 끝
 
-
   return (
     <div className={styles.App}>
       <nav className={navStyles.nav}>
-        <div className={navStyles['nav-container']}>
+        <div className={navStyles["nav-container"]}>
           <a className={navStyles.logo} href="/community">
-            <Image className={navStyles['logo-img']} src={logo} alt="Logo" />
+            <Image className={navStyles["logo-img"]} src={logo} alt="Logo" />
           </a>
-          <ul className={navStyles['nav-list']}>
+          <ul className={navStyles["nav-list"]}>
             <li className={navStyles.community}>
               <a href="community">Community</a>
             </li>
@@ -265,7 +272,7 @@ function Community() {
         </div>
       </nav>
 
-      <div className={styles['post-container']}>
+      <div className={styles["post-container"]}>
         <div className={styles.newPostContainer}>
           <div className={styles.newPost}>
             <button className={styles.newPostBtn} onClick={openModal}>
@@ -280,7 +287,11 @@ function Community() {
 
       {showModal && (
         <div className={styles.modal}>
-          <form ref={modal} onSubmit={handleSubmit} className={styles.modalform}>
+          <form
+            ref={modal}
+            onSubmit={handleSubmit}
+            className={styles.modalform}
+          >
             <input
               type="text"
               placeholder="제목"
@@ -303,8 +314,14 @@ function Community() {
                 onChange={handleRoomIDChange}
                 className={roomIDInputStyles}
               />
-              {roomIdError && <p className={styles.errorMessage}>* 해당 스터디룸 ID는 이미 존재합니다.</p>}
-              {missingFieldsError && <p className={styles.errorMessage}>* 모든 칸을 입력해주세요.</p>}
+              {roomIdError && (
+                <p className={styles.errorMessage}>
+                  * 해당 스터디룸 ID는 이미 존재합니다.
+                </p>
+              )}
+              {missingFieldsError && (
+                <p className={styles.errorMessage}>* 모든 칸을 입력해주세요.</p>
+              )}
             </div>
             <div className={styles.buttonContainer}>
               <button type="submit" disabled={roomIdError}>
